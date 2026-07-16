10b28cd6009668b5f96ac8404c5c1270e8f24a36
muhammad <muhammadyoqubov70>
2026-07-16 14:29:50 +0500

    ..

--- src/pages/layouts/PubliLayout.jsx
+++ src/pages/layouts/PubliLayout.jsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+
 function PubliLayout() {
   return (
     <div>
+      <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
+
 export default PubliLayout;
