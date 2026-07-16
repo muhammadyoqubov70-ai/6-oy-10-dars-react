b283770d7e4522fdddffb211ed23b2fef9761736
muhammad <muhammadyoqubov70>
2026-07-16 15:24:45 +0500

    loginpage edit3

--- src/pages/public/Posts.jsx
+++ src/pages/public/Posts.jsx
@@ -1,4 +1,19 @@
+import posts from "../../data/Card";
+import PostCard from "../../components/PostCard";
+
 function Posts() {
-  return <div>Posts sahifasi</div>;
+  return (
+    <div className="posts-page center">
+      <h1>Explore Our Posts</h1>
+      <p>Discover amazing content from talented writers across various topics</p>
+
+      <div className="posts-grid">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+    </div>
+  );
 }
+
 export default Posts;
